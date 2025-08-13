18c13d2d6b328e74127a7cccf161c7016c7fbc80
Sophie Zhou <sophiezy@umich.edu>
2025-08-13 22:26:52 +0000

    live!

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,15 @@ module.exports = {
         header: ["'Lora'", "serif"],
         body: ["'Roboto'", "sans-serif"],
       },
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
     },
   },
   plugins: [],
